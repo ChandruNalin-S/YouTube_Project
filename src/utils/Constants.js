00005df28a2b4ee1f8fdc34889e5dc158b833d0c
Chandru s <chandrusakthivelc@gmail.com>
2024-11-30 14:40:11 +0530

    secure api key in env file

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -8,7 +8,7 @@ export const YOUTUBE_URL = "https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47b
 export const USER_ICON_URL = "https://static.vecteezy.com/system/resources/previews/030/750/807/non_2x/user-icon-in-trendy-outline-style-isolated-on-white-background-user-silhouette-symbol-for-your-website-design-logo-app-ui-illustration-eps10-free-vector.jpg";
 
 
-const GOOGLE_API_KEY = "AIzaSyCh9Md65JG4eeGPhrS6A4ir60sdmfjsHl0";
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
 
-export const YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+ GOOGLE_API_KEY;
+export const YOUTUBE_VIDEOS_API = process.env.REACT_APP_YOUTUBE_VIDEOS_API+ GOOGLE_API_KEY;
